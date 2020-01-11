8880349b2ccf0a494a796c6f7bd9aa599b08a941
kmaasrud <kmaasrud@outlook.com>
2020-01-11 22:53:45 +0100

    Written stop ID finder.

---
+++ entur_stopid.py
@@ -0,0 +1,20 @@
+import json
+import urllib.request
+
+def stopid(query):
+    """Returns list of NSR ID's matching input string."""
+    url = "https://api.entur.io/geocoder/v1/autocomplete?text="+query+"&lang=nb"
+    with urllib.request.urlopen(url) as request:
+        json_data = json.loads(request.read().decode())
+
+    features = json_data['features']
+
+    places = []
+    for place in features:
+        if place['properties']['category'][0] in ['metroStation', 'onstreetBus', 'busStation', 'railStation']:
+            places.append(place['properties']['id'])
+
+    return places
+
+if __name__ == "__main__":
+    print(stopid('nittedal'))
