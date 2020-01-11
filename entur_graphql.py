7fcebc76a1961820714b7da1dbed6379558e9401
kmaasrud <kmaasrud@outlook.com>
2020-01-11 22:20:19 +0100

    Function interacting with Entur GraphQL API and returning dictionary

---
+++ entur_graphql.py
@@ -0,0 +1,60 @@
+import requests
+import json
+import pandas as pd
+
+def realtime_data(stops):
+  """Returns dictionary of realtime transport data from list of Entur stop id's"""
+
+  query_template = """{{
+    stopPlace(id: "{}") {{
+      name
+      estimatedCalls(timeRange: 72100, numberOfDepartures: 10) {{
+        aimedArrivalTime
+        expectedArrivalTime
+        quay {{
+          publicCode
+        }}
+        destinationDisplay {{
+          frontText
+        }}
+        serviceJourney {{
+          journeyPattern {{
+            line {{
+              publicCode
+            }}
+          }}
+        }}
+      }}
+    }}
+  }}"""
+
+  url = 'https://api.entur.io/journey-planner/v2/graphql'
+  stops = [
+    {'name': 'Helsfyr', 'id': "NSR:StopPlace:59516"}, 
+    {'name': 'Fyrstikktorget', 'id': "NSR:StopPlace:60245"}
+  ]
+
+  stops_data = {}
+  for i in range(len(stops)):
+    query = query_template.format(stops[i]['id'])
+    r = requests.post(url, json={'query': query})
+    json_data = json.loads(r.text)['data']['stopPlace']
+    name = json_data['name']
+
+    s = []
+    for call in json_data['estimatedCalls']:
+        dictio = {
+            'platform': call['quay']['publicCode'],
+            'line': call['serviceJourney']['journeyPattern']['line']['publicCode']+" "+call['destinationDisplay']['frontText'],
+            'aimedArrivalTime': call['aimedArrivalTime'],
+            'expectedArrivalTime': call['expectedArrivalTime']
+        }
+        s.append(dictio)
+
+    stops_data[name] = s
+
+  return stops_data
+
+if __name__ == "__main__":
+    stops = ["NSR:StopPlace:59516", "NSR:StopPlace:60245"] # Example list. Stop id's are found at the end of urls from Entur journey planner.
+    print(realtime_data(stops))
