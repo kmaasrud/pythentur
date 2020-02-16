142783580282bf2de0dc28aa1a15d560e52c95af
kmaasrud <kmaasrud@outlook.com>
2020-02-16 12:16:53 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pythentur",
-    version="0.0.5",
+    version="0.0.6",
     author="Knut Magnus Aasrud",
     author_email="kmaasrud@outlook.com",
     description="Simplified data retrieval from Entur in Python",
