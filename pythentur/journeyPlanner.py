1ef7f14ab2389052e551f61488e89c44a377e90e
kmaasrud <kmaasrud@outlook.com>
2020-02-16 19:28:17 +0100

    Get method written

--- pythentur/journeyPlanner.py
+++ pythentur/journeyPlanner.py
@@ -1,17 +1,42 @@
 import requests
 import json
 import sys
-from datetime import datetime
+from datetime import datetime, timezone
 
 class Journey():
     def __init__(self, fromPlace, toPlace, header, time = None, noDepartures = 20):
+        self.fromPlace = fromPlace
+        self.toPlace = toPlace
+        self.header = header
         self.time = time
         self.query_formatter = {'from': fromPlace, 'to': toPlace, 'noDepartures': noDepartures}
 
     def get(self):
+        if self.time is None: self.query_formatter['time'] = datetime.now(timezone.utc).strftime(iso_datestring)
         query = query_template.format(self.query_formatter)
-        r = requests.post(api_url, json={'query': query}, headers={'ET-Client-Name': 'kmaasrud - pythentur'})
-        json_data = json.loads(r.text)['data']
+        r = requests.post(api_url, json={'query': query}, headers={'ET-Client-Name': self.header})
+        json_data = json.loads(r.text)['data']['trip']['tripPatterns']
+
+        data = []
+        for trip in json_data:
+            duration = trip['duration']
+            legs = []
+            for leg in trip['legs']:
+                legs.append({
+                    'transportMode': leg['mode'],
+                    'aimedStartTime': datetime.strptime(leg['aimedStartTime'], iso_datestring),
+                    'expectedStartTime': datetime.strptime(leg['expectedStartTime'], iso_datestring),
+                    'lineName': leg['fromEstimatedCall']['destinationDisplay']['frontText'],
+                    'lineNumber': leg['line']['publicCode'],
+                    'lineColor': "#" + leg['line']['presentation']['colour'],
+                    'fromName': leg['fromPlace']['quay']['stopPlace']['name'],
+                    'fromId': leg['fromPlace']['quay']['stopPlace']['id'],
+                    'toName': leg['toPlace']['quay']['stopPlace']['name'],
+                    'toId': leg['toPlace']['quay']['stopPlace']['id']
+                })
+            data.append({'duration': duration, 'legs': legs})
+        
+        return data
 
 # TODO: Preferred or banned transport modes.
 query_template = """{{
