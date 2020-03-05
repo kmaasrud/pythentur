b2780d80e468cd95a07bd89d9a45bf481e14e288
kmaasrud <kmaasrud@outlook.com>
2020-03-05 15:53:45 +0100

    Directory restructure and gathered constants

---
+++ pythentur/helpers/constants.py
@@ -0,0 +1,80 @@
+API_URL = 'https://api.entur.io/journey-planner/v2/graphql'
+
+GEOCODER_URL = 'https://api.entur.io/geocoder/v1'
+
+ISO_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
+
+QUERY_STOP_PLACE = """{{
+  stopPlace(id: \"{}\") {{
+      name
+      estimatedCalls(timeRange: 72100, numberOfDepartures: {}) {{
+        aimedArrivalTime
+        expectedArrivalTime
+        quay {{
+          publicCode
+        }}
+        destinationDisplay {{
+          frontText
+        }}
+        serviceJourney {{
+          journeyPattern {{
+            line {{
+              publicCode
+            }}
+          }}
+        }}
+      }}
+    }}
+  }}"""
+
+QUERY_JOURNEY = """{{
+  trip(
+    from: {{
+      place: \"{from}\"
+    }}
+    to: {{
+      place: \"{to}\"
+    }}
+    numTripPatterns: {noDepartures}
+    dateTime: \"{time}\"
+    minimumTransferTime: 180
+  )
+  {{
+    tripPatterns {{
+      duration
+      legs {{
+        mode
+        aimedStartTime
+        expectedStartTime
+        fromEstimatedCall {{
+          destinationDisplay {{
+            frontText
+          }}
+        }}
+        line {{
+          publicCode
+          name
+          presentation {{
+            colour
+          }}
+        }}
+        fromPlace{{
+          quay {{
+            stopPlace {{
+              name
+              id
+            }}
+          }}
+        }}
+        toPlace {{
+          quay {{
+            stopPlace {{
+              name
+              id
+            }}
+          }}
+        }}
+      }}
+    }}
+  }}
+}}"""
--- pythentur/helpers/nsrGet.py
+++ pythentur/helpers/nsrGet.py
@@ -1,6 +1,5 @@
 import json
 import urllib.request
-import sys
 
 # TODO: Unicode issues
 
