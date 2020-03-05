164a9bd7c44cd64d7398d5b9d730dfb6a2e57593
kmaasrud <kmaasrud@outlook.com>
2020-03-05 15:54:46 +0100

    Added Location class

---
+++ pythentur/Location.py
@@ -0,0 +1,38 @@
+import json
+import urllib.request as urqst
+from helpers.constants import GEOCODER_URL
+
+class Location:
+    """Doc"""
+    def __init__(self, lat, lon, header):
+        self.lat = lat
+        self.lon = lon
+
+        url = GEOCODER_URL + '/reverse?point.lat={}&point.lon={}&size=1&lang=en'.format(str(lat),str(lon))
+        req = urqst.Request(url, headers={'ET-Client-Name': header})
+        with urqst.urlopen(req) as request:
+            json_data = json.loads(request.read().decode())
+
+        properties = json_data['features'][0]['properties']
+        self.name = properties['name']
+        self.county = properties['county']
+        self.locality = properties['locality']
+        self.borough = properties['borough']
+
+    @classmethod
+    def from_string(cls, query, header):
+        query = query.replace(" ", "%20")
+        url = GEOCODER_URL + '/autocomplete?text={}&size=1&lang=en'.format(query)
+        req = urqst.Request(url, headers={'ET-Client-Name': header})
+        with urqst.urlopen(req) as request:
+            json_data = json.loads(request.read().decode())
+
+        coords = json_data['features'][0]['geometry']['coordinates']
+        return cls(coords[1], coords[0], header)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+if __name__ == "__main__":
+    hjemme = Location.from_string('Hulderveien 7', 'kmaasrud - pythentur')
+    print(hjemme['county'])
